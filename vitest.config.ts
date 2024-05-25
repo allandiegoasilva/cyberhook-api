65c47bbd3798c86e11ea9820058b738446f077ae
Allan Diego <allandiego252@gmail.com>
2024-05-25 11:05:42 -0300

    config: add vitest alias for any file from src folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
+import { resolve } from 'path'
 import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude],
   },
+  resolve: {
+    alias: [
+      {
+        replacement: resolve(__dirname, "./src/"),
+        find: /[@/*]/g
+      }
+    ]
+  }
 })
