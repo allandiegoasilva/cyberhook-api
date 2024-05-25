71993aa137725b12070653f59287574ca05f4531
Allan Diego <allandiego252@gmail.com>
2024-05-25 11:28:11 -0300

    config: add vitest plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'path'
 import { configDefaults, defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     exclude: [...configDefaults.exclude],
   },
